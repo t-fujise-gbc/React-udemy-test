0b9faec32559d4bf74f1bc1ef03b02adbf80de95
FUJISE Takumi <138739798+t-fujise-gbc@users.noreply.github.com>
2023-08-02 21:20:14 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import ColorfulMessage from './components/ColorfulMessage'
+
+const App = () => {
+  const onClickCountUp = () => {
+    // setNum(num + 1)
+    // setNum(num + 1)
+    setNum((prev) => prev + 1)
+    setNum((prev) => prev + 1)
+  }
+  const [num, setNum] = useState(0)
+
+  return (
+    <>
+      <h1 style={{ color: 'red' }}>こんにちは</h1>
+      <ColorfulMessage color='blue'>
+        お元気ですか？
+      </ColorfulMessage>
+      <ColorfulMessage color='pink'>元気です！</ColorfulMessage>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+      <p>{num}</p>
+    </>
+  )
+}
+
+export default App;
